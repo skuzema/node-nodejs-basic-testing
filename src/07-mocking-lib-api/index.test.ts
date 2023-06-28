0c720fe8dcbcee5c5dc759923f848498e09a29cb
skuzema <s.kuzema@gmail.com>
2023-06-28 22:22:46 +0300

    feat: Snapshot testing

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,76 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+// import axios, { AxiosInstance, AxiosResponse } from 'axios';
+// import { throttledGetDataFromApi } from './index';
 
-describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    // Write your test here
-  });
+// jest.mock('axios');
+// describe('throttledGetDataFromApi', () => {
+//   test('should create instance with provided base url', async () => {
+//     const axiosInstanceMock = {
+//       get: jest.fn(),
+//       defaults: {},
+//       interceptors: {},
+//       getUri: jest.fn(),
+//       request: jest.fn(),
+//       // Include other necessary properties/methods here
+//     } as unknown as AxiosInstance;
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
-  });
+//     jest.spyOn(axios, 'create').mockReturnValue(axiosInstanceMock);
 
-  test('should return response data', async () => {
-    // Write your test here
-  });
-});
+//     const responseData = 'mocked data';
+//     const response: AxiosResponse = {
+//       data: responseData,
+//       status: 200,
+//       statusText: 'OK',
+//       headers: {}, // Empty headers object
+//       config: {},
+//     };
+
+//     axios.get.mockResolvedValueOnce(response);
+
+//     const result = await throttledGetDataFromApi('/posts');
+
+//     expect(axios.create).toHaveBeenCalledWith({
+//       baseURL: 'https://jsonplaceholder.typicode.com',
+//     });
+
+//     expect(result).toEqual(responseData);
+//   });
+
+//   test('should perform request to correct provided url', async () => {
+//     const axiosClient = axios.create();
+//     const getSpy = jest
+//       .spyOn(axiosClient, 'get')
+//       .mockResolvedValueOnce({ data: 'test data' });
+
+//     const axiosCreateSpy = jest
+//       .spyOn(axios, 'create')
+//       .mockReturnValueOnce(axiosClient);
+
+//     const result = await throttledGetDataFromApi('/posts');
+
+//     expect(axiosCreateSpy).toHaveBeenCalledWith({
+//       baseURL: 'https://jsonplaceholder.typicode.com',
+//     });
+//     expect(getSpy).toHaveBeenCalledWith('/posts');
+//     expect(result).toEqual('test data');
+//   });
+
+//   test('should return response data', async () => {
+//     const axiosClient = axios.create();
+//     const getSpy = jest
+//       .spyOn(axiosClient, 'get')
+//       .mockResolvedValueOnce({ data: 'response data' });
+
+//     const axiosCreateSpy = jest
+//       .spyOn(axios, 'create')
+//       .mockReturnValueOnce(axiosClient);
+
+//     const result = await throttledGetDataFromApi('/posts');
+
+//     expect(axiosCreateSpy).toHaveBeenCalledWith({
+//       baseURL: 'https://jsonplaceholder.typicode.com',
+//     });
+//     expect(getSpy).toHaveBeenCalledWith('/posts');
+//     expect(result).toEqual('response data');
+//   });
+// });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,34 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = [1, 2, 3];
+    const result = generateLinkedList(values);
+
+    const expected = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    expect(result).toStrictEqual(expected);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = [2, 3, 4, 5, 6];
+    const result = generateLinkedList(values);
+
+    expect(result).toMatchSnapshot();
   });
 });
